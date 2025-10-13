9d3c9154b0100fdf22392cacab2267ca48c76d29
belan <smakki.92@gmail.com>
2025-10-13 17:37:13 +0300

    Added quartz job

---
+++ src/backend/FinanceManager.TransactionsService/FinanceManager.TransactionsService.API/Extensions/QuartzExtensions.cs
@@ -0,0 +1,31 @@
+﻿using FinanceManager.TransactionsService.Abstractions.Services;
+using FinanceManager.TransactionsService.Implementations.Jobs;
+using FinanceManager.TransactionsService.Implementations.Services;
+using Quartz;
+using Quartz.Spi;
+
+namespace FinanceManager.TransactionsService.API.Extensions;
+
+public static class QuartzExtensions
+{
+    public static IServiceCollection AddTransactionDataLoaderJob(this IServiceCollection services)
+    {
+        services.AddScoped<IExternalDataLoaderService, ExternalDataLoaderService>();
+
+        services.AddQuartz(q =>
+        {
+            //q.UseMicrosoftDependencyInjectionScopedJobFactory();
+
+            var jobKey = new JobKey(nameof(ExternalDataLoaderJob));
+            q.AddJob<ExternalDataLoaderJob>(opts => opts.WithIdentity(jobKey));
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity($"{nameof(ExternalDataLoaderJob)}-trigger")
+                .WithSimpleSchedule(x => x.WithIntervalInHours(1).RepeatForever()));
+        });
+
+        services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+        return services;
+    }
+}
---
+++ src/backend/FinanceManager.TransactionsService/FinanceManager.TransactionsService.Implementations/Services/ExternalDataLoaderService.cs
@@ -0,0 +1,30 @@
+﻿using FinanceManager.TransactionsService.Abstractions.Repositories;
+using FinanceManager.TransactionsService.Abstractions.Services;
+
+namespace FinanceManager.TransactionsService.Implementations.Services;
+
+public class ExternalDataLoaderService(
+    ITransactionHolderRepository holderRepository,
+    ITransactionAccountRepository accountRepository,
+    IAccountTypeRepository accountTypeRepository,
+    ITransactionCategoryRepository categoryRepository,
+    ITransactionCurrencyRepository currencyRepository)
+    : IExternalDataLoaderService
+{
+    private readonly ITransactionHolderRepository _holderRepository = holderRepository;
+    private readonly ITransactionAccountRepository _accountRepository = accountRepository;
+    private readonly IAccountTypeRepository _accountTypeRepository = accountTypeRepository;
+    private readonly ITransactionCategoryRepository _categoryRepository = categoryRepository;
+    private readonly ITransactionCurrencyRepository _currencyRepository = currencyRepository;
+    
+    public async Task LoadTransactionHoldersAsync(CancellationToken cancellationToken)
+    {
+        // Здесь вызываем внешний API, получаем данные и сохраняем через репозиторий
+    }
+
+    public async Task LoadTransactionsAccountsAsync(CancellationToken cancellationToken) { /* аналогично */ }
+    public async Task LoadAccountTypesAsync(CancellationToken cancellationToken) { /* аналогично */ }
+    public async Task LoadCategoriesAsync(CancellationToken cancellationToken) { /* аналогично */ }
+    public async Task LoadCurrenciesAsync(CancellationToken cancellationToken) { /* аналогично */ }
+
+}
